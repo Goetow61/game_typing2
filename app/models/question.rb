f59dd8c7d2c060230316e87a96a12a4cc280c76b
techexpert65-19894 <u_ku_su_tu_nu@hotmail.co.jp>
2020-03-18 03:31:53 +0900

    デプロイエラー少しずつ様子見3
    ・モデルにuploader追加
    ・要らないビュー削除
    ・ビュー追加

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,7 @@
 class Question < ApplicationRecord
   belongs_to :user
   has_many :results
+  mount_uploader :src, QuestionUploader
 
   # rails enumで検索 単語が0、文章が1で入る
   enum category: [:'英語-単語', :'英語-文章']

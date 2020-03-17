97702dab427bd90dc2c72f74b3c1e44afdcf3355
techexpert65-19894 <u_ku_su_tu_nu@hotmail.co.jp>
2020-03-18 01:16:26 +0900

    既定ユーザの検索及びSEEDファイルの修正
    既定ユーザをid=1で指定していたが、いつ登録してもいつ検索しても
    問題無いように、メールアドレスから検索することにした。

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,8 @@
 User.create(nickname: '既定ユーザ', email: ENV['GMAIL_ADDRESS'], password: [*'A'..'Z', *'a'..'z', *0..9].shuffle[0..7].join )
 
 # 既定のユーザがデフォルトで登録しておく問題
+id = User.where(email: ENV['GMAIL_ADDRESS']).select(:id)
+binding.pry
 title = ['abide - certification 200語','certify - drill 200語','drought - indoor 200語','induction - painter 200語','pamphlet - satellite 200語','satisfactory - zoo 259語','英語例文 200件 1','英語例文 200件 2','英語例文 200件 3','英語例文 200件 4']
 overview = ['THE TOEIC SERVICE LIST 1.1の内、↵abide　から　certification　までの↵200語を抜き出したもの','THE TOEIC SERVICE LIST 1.1の内、↵certify　から　drill　までの↵200語を抜き出したもの','THE TOEIC SERVICE LIST 1.1の内、↵drought　から　indoor　までの↵200語を抜き出したもの','THE TOEIC SERVICE LIST 1.1の内、↵induction　から　painter　までの↵200語を抜き出したもの','THE TOEIC SERVICE LIST 1.1の内、↵pamphlet　から　satellite　までの↵200語を抜き出したもの','THE TOEIC SERVICE LIST 1.1の内、↵satisfactory　から　zoo　までの↵259語を抜き出したもの','Tatoeba 文章と翻訳のコレクション(https://tatoeba.org/jpn/)↵から英語の例文を取り出して、200件毎に保存したものです。↵↵日本語は、英語の例文をGoogle翻訳したものなので、↵正しく訳せていない場合があります。','Tatoeba 文章と翻訳のコレクション(https://tatoeba.org/jpn/)↵から英語の例文を取り出して、200件毎に保存したものです。↵↵日本語は、英語の例文をGoogle翻訳したものなので、↵正しく訳せていない場合があります。','Tatoeba 文章と翻訳のコレクション(https://tatoeba.org/jpn/)↵から英語の例文を取り出して、200件毎に保存したものです。↵↵日本語は、英語の例文をGoogle翻訳したものなので、↵正しく訳せていない場合があります。','Tatoeba 文章と翻訳のコレクション(https://tatoeba.org/jpn/)↵から英語の例文を取り出して、200件毎に保存したものです。↵↵日本語は、英語の例文をGoogle翻訳したものなので、↵正しく訳せていない場合があります。','THE TOEIC SERVICE LIST 1.1の内、↵abide　から　certification　までの↵200語を抜き出したもの']
 src = ['word_001.csv','word_002.csv','word_003.csv','word_004.csv','word_005.csv','word_006.csv','sentence_001.csv','sentence_002.csv','sentence_003.csv','sentence_004.csv']
@@ -18,6 +20,7 @@ category = [0,0,0,0,0,0,1,1,1,1]
 
 count = 0
 while(count < title.length) do
-  Question.create(title: title[count], overview: overview[count], src: src[count], category: category[count], user_id: 1 )
+  Question.create(title: title[count], overview: overview[count], src: src[count], category: category[count], user_id: id.ids[0] )
+  # binding.pry
   count = count + 1
 end

bbe38d20281406fd0aaa0a6bf252a7c98895ae00
techexpert65-19894 <u_ku_su_tu_nu@hotmail.co.jp>
2020-03-18 01:20:19 +0900

    Update seeds.rb
    binding.pryの抜き忘れ

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,6 @@ User.create(nickname: '既定ユーザ', email: ENV['GMAIL_ADDRESS'], password: 
 
 # 既定のユーザがデフォルトで登録しておく問題
 id = User.where(email: ENV['GMAIL_ADDRESS']).select(:id)
-binding.pry
 title = ['abide - certification 200語','certify - drill 200語','drought - indoor 200語','induction - painter 200語','pamphlet - satellite 200語','satisfactory - zoo 259語','英語例文 200件 1','英語例文 200件 2','英語例文 200件 3','英語例文 200件 4']
 overview = ['THE TOEIC SERVICE LIST 1.1の内、↵abide　から　certification　までの↵200語を抜き出したもの','THE TOEIC SERVICE LIST 1.1の内、↵certify　から　drill　までの↵200語を抜き出したもの','THE TOEIC SERVICE LIST 1.1の内、↵drought　から　indoor　までの↵200語を抜き出したもの','THE TOEIC SERVICE LIST 1.1の内、↵induction　から　painter　までの↵200語を抜き出したもの','THE TOEIC SERVICE LIST 1.1の内、↵pamphlet　から　satellite　までの↵200語を抜き出したもの','THE TOEIC SERVICE LIST 1.1の内、↵satisfactory　から　zoo　までの↵259語を抜き出したもの','Tatoeba 文章と翻訳のコレクション(https://tatoeba.org/jpn/)↵から英語の例文を取り出して、200件毎に保存したものです。↵↵日本語は、英語の例文をGoogle翻訳したものなので、↵正しく訳せていない場合があります。','Tatoeba 文章と翻訳のコレクション(https://tatoeba.org/jpn/)↵から英語の例文を取り出して、200件毎に保存したものです。↵↵日本語は、英語の例文をGoogle翻訳したものなので、↵正しく訳せていない場合があります。','Tatoeba 文章と翻訳のコレクション(https://tatoeba.org/jpn/)↵から英語の例文を取り出して、200件毎に保存したものです。↵↵日本語は、英語の例文をGoogle翻訳したものなので、↵正しく訳せていない場合があります。','Tatoeba 文章と翻訳のコレクション(https://tatoeba.org/jpn/)↵から英語の例文を取り出して、200件毎に保存したものです。↵↵日本語は、英語の例文をGoogle翻訳したものなので、↵正しく訳せていない場合があります。','THE TOEIC SERVICE LIST 1.1の内、↵abide　から　certification　までの↵200語を抜き出したもの']
 src = ['word_001.csv','word_002.csv','word_003.csv','word_004.csv','word_005.csv','word_006.csv','sentence_001.csv','sentence_002.csv','sentence_003.csv','sentence_004.csv']
